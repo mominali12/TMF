4da4f5b5551b2dccbb2866914b34095531b7ec60
Osama Rehman <osamarehman1@outlook.com>
2021-01-09 15:49:34 +0500

    new push

--- server/controller/home/controller.js
+++ server/controller/home/controller.js
@@ -33,9 +33,48 @@ class Controller
         res.redirect('/login');
     }
 
+
+    async GetCompleted (req,res)
+    {
+      //console.log("Hello" + process.env.ACTIVE_USER);
+      if(process.env.ACTIVE_USER != "")
+      {
+        
+        fs.readFile(path.join(__dirname, '/../../../client_end/completed.html'), null,function (error, data)
+        {
+          //console.log(path.join(__dirname, '/../../../client_side/login_signup.html'));
+          if (error)
+          {
+              res.writeHead(404);
+              res.write('Whoops! File not found!');
+          }
+          else
+          {
+            res.writeHead(200, {
+              'Content-Type': 'text/html',
+            });
+            res.write(data);
+            //res.json(orders);
+          }
+          res.end();
+        });
+      }
+      else
+        res.redirect('/login');
+    }
+
     async GetOrders(req,res)
     {
       const orders = await HomeService.getHomeData();
+      //orders.push({orders[0][0]): typeof(orders[0][0])});
+      var column_types = {};
+      console.log(orders[0]);
+      for(let k in orders[0].toJSON())
+      {
+        console.log(k + ':' +typeof(orders[0][k]));
+      }      
+        //column_types[k] = typeof(k.value);
+      //console.log(column_types);
       orders.push({'store_logo_path' : "assets/" + process.env.ACTIVE_USER_ID + process.env.ACTIVE_USER + '.png',
                    'store_logo' : process.env.ACTIVE_USER_ID + process.env.ACTIVE_USER});
       // console.log(orders);
