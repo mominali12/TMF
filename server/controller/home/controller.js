c470d0af59d507098155c36b0be35764348f3143
Momin Ali <mominali1296@gmail.com>
2021-01-19 19:11:00 +0500

    Added new customer form

--- server/controller/home/controller.js
+++ server/controller/home/controller.js
@@ -2,6 +2,15 @@ const HomeService = require('../../services/homeservice');
 const fs = require('fs');
 const path = require('path');
 
+//----------------------------------Added
+const mongodb = require('mongodb')
+    // const app = express()
+    // const router = express.Router()
+    // const mongoClient = mongodb.MongoClient
+const binary = mongodb.Binary;
+//----------------------------------------
+
+
 class Controller {
     async GetHome(req, res) {
         //console.log("Hello" + process.env.ACTIVE_USER);
@@ -127,6 +136,64 @@ class Controller {
         }
 
     }
+
+    async NewCustomerForm(req, res) {
+        if (process.env.ACTIVE_USER != "") {
+
+            fs.readFile(path.join(__dirname, '/../../../client_end/newcustomer.html'), null, function(error, data) {
+                //console.log(path.join(__dirname, '/../../../client_side/login_signup.html'));
+                if (error) {
+                    res.writeHead(404);
+                    res.write('Whoops! File not found!');
+                } else {
+                    res.writeHead(200, {
+                        'Content-Type': 'text/html',
+                    });
+                    res.write(data);
+                    //res.json(orders);
+                }
+                res.end();
+            });
+        } else
+            res.redirect('/login');
+    }
+
+    async SaveCustomerData(req, res) {
+        if (process.env.ACTIVE_USER != "") {
+
+            let file = { customername: req.body.customername, productname: req.body.productname, filename: req.body.filename, file: binary(req.files.uploadedFile.data) }
+            console.log(file)
+                // insertFile(file, res)
+
+            let result = await HomeService.SaveCustomerData(req.body);
+            if (result)
+                res.status(200).redirect('/newcustomer');
+            else
+                res.status(400).send(result);
+        } else
+            res.redirect('/login');
+    }
+
+    async insertFile(file, res) {
+        // mongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true }, (err, client) => {
+        //     if (err) {
+        //         return err
+        //     } else {
+        //         let db = client.db('uploadDB')
+        //         let collection = db.collection('files')
+        //         try {
+        //             collection.insertOne(file)
+        //             console.log('File Inserted')
+        //         } catch (err) {
+        //             console.log('Error while inserting:', err)
+        //         }
+        //         client.close()
+        //         res.redirect('/')
+        //     }
+
+        // })
+    }
+
 }
 
 module.exports = new Controller();
