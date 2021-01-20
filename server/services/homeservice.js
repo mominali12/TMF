1a377bf9af9e671391114656e64413ef4445105c
Osama Rehman <osamarehman1@outlook.com>
2021-01-20 15:11:46 +0500

    new changes in SaveCustomerData

--- server/services/homeservice.js
+++ server/services/homeservice.js
@@ -10,11 +10,11 @@ class HomeDatabase {
         if (data.customer_name === "" || data.customer_name === undefined || data.customer_name == null)
             return false;
         console.log(data);
-        let file = { customer_name: data.customer_name, customer_address: data.customer_address, customer_email: data.customer_email, customer_contact_no: data.customer_contact_no, filename_1: data.filename_1, file_1: binary(files.uploadedFile.data) }
+        let file = { customer_name: data.customer_name, customer_address: data.customer_address, customer_email: data.customer_email, customer_contact_no: data.customer_contact_no, filename_1: data.filename_1, file_1: binary(files.uploadedFile.data), user_id : process.env.ACTIVE_USER_ID }
         console.log(file)
         console.log("Saving Customer Data ...");
         await Customers.deleteMany({ $and: [{ user_id: process.env.ACTIVE_USER_ID }, { customer_name: data.customer_name }] });
-        await Customers.insertMany(data);
+        await Customers.insertMany(file);
         console.log("Success!");
         return true;
     }
